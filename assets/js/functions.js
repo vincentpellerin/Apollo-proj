630cd8e0a0cc9abb25ef2f794fd6708c119d526c
vincentpellerin <pellerinmartinv@hotmail.com>
2022-04-07 15:10:30 -0400

    first commit

---
+++ assets/js/functions.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+	
+function nav(){
+	
+	$('.nav-toggle').click(function(){
+		
+		$('.nav').toggleClass('open');
+		
+	});
+	
+}
+
+function smoothScroll(){
+// Faire la fonction ici
+}
+
+function fullSlider(){
+// Faire le code du slider ici
+}
+
+
+nav();
+
+smoothScroll();
+
+fullSlider();
+
+});
